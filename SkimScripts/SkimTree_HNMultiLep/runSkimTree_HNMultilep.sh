7d2f1b1ed8ebee5a4acfa7c0c236849f327c3a8a
jalmond <jalmond@cern.ch>
2023-05-18 17:18:50 +0900

    Add 2018 skims

--- SkimScripts/SkimTree_HNMultiLep/runSkimTree_HNMultilep.sh
+++ SkimScripts/SkimTree_HNMultiLep/runSkimTree_HNMultilep.sh
@@ -3,65 +3,51 @@ qrundir=runSkims
 mcpath=${SKFlat_WD}/SkimScripts/${analyzer}/Bkg/
 datapath=${SKFlat_WD}/SkimScripts/${analyzer}/data_lists_multilep/
 sigpath=${SKFlat_WD}/SkimScripts/${analyzer}/Signals/
-njobs=400
+njobs=600
 njobs_data=100
 nmax=350
 skim=' '
-declare  -a era_list=("2016postVFP" "2016preVFP" "2017" "2018")
+
+declare  -a era_list=("2016postVFP" "2016preVFP"  "2017" "2018")
 
 
-if [[ $1 == "ALL" ]]; then
-    for i in "${era_list[@]}"
-    do
-        SKFlat.py -a $analyzer  -l $datapath/DATA_${i}.txt  -n ${njobs_data}  --nmax ${nmax}   -e ${i}  & 
-        SKFlat.py -a $analyzer  -l $datapath/DATA_l_${i}.txt   -n ${njobs_data}  --nmax ${nmax}  -e ${i} &
-	SKFlat.py -a $analyzer  -l $datapath/DATA_${i}EMu.txt  -n ${njobs_data}  --nmax ${nmax}   -e ${i} 
-	
-	SKFlat.py -a $analyzer  -l ${mcpath}/MC.txt  -n 50  --nmax ${nmax}   -e ${i} &
-        SKFlat.py -a $analyzer  -l ${mcpath}/MC2.txt  -n 300  --nmax ${nmax}   -e ${i} &  
-        SKFlat.py -a $analyzer  -l ${mcpath}/MC3.txt  -n 300  --nmax ${nmax}   -e ${i}  &
-        SKFlat.py -a $analyzer  -l ${mcpath}/MC4.txt  -n 50  --nmax ${nmax}   -e ${i}  
-
-
-    done
+if [[ $1 == "TEST" ]]; then
+    SKFlat.py -a $analyzer -i SSWWTypeI_SF_M1000_private -n  100  -e 2017&
 fi
+
 
 if [[ $1 == "DATA" ]]; then
     for i in "${era_list[@]}"
     do
-        SKFlat.py -a $analyzer  -l $datapath/DATA_${i}.txt  -n ${njobs_data}  --nmax ${nmax}   -e ${i}   &
-        SKFlat.py -a $analyzer  -l $datapath/DATA_l_${i}.txt   -n ${njobs_data}  --nmax ${nmax}  -e ${i}
-    done
-fi
-
-if [[ $1 == "DATAEMu" ]]; then
-    for i in "${era_list[@]}"
-    do
-        SKFlat.py -a $analyzer  -l $datapath/DATA_${i}EMu.txt  -n ${njobs_data}  --nmax 100   -e ${i} &
-
+        SKFlat.py -a $analyzer  -l $datapath/DATA_${i}.txt  -n ${njobs_data}  --nmax ${nmax}   -e ${i}    &
+        SKFlat.py -a $analyzer  -l $datapath/DATA_l_${i}.txt   -n ${njobs_data}  --nmax ${nmax}    -e ${i} &
     done
 
 fi
 if [[ $1 == "MC" ]]; then
-    for i in "${era_list[@]}"
-    do
-	SKFlat.py -a $analyzer  -l ${mcpath}/MC5.txt  -n 200  --nmax ${nmax}   -e ${i}  &
-		
-    done
+    SKFlat.py -a $analyzer  -l ${mcpath}/2016postVFP/QCD.txt -n 2  --nmax ${nmax}   -e ${i} 
 
 fi
 
 
-if [[ $1 == "IND" ]]; then
-
-        
+if [[ $1 == "Signal" ]]; then
     for i in "${era_list[@]}"
     do
-	SKFlat.py -a $analyzer  -i WZTo3LNu_mllmin0p1_powheg  -n 200  --nmax ${nmax}   -e  ${i}  &
-	SKFlat.py -a $analyzer  -i DYJets10to50_MG  -n 150  --nmax ${nmax}   -e ${i}  &
-	SKFlat.py -a $analyzer  -l  ${mcpath}/${i}/QCD.txt -n 20  --nmax ${nmax}   -e ${i}  &
+	SKFlat.py -a $analyzer  -l ${sigpath}/Signal.txt  -n 100  --nmax 100   -e ${i}   
+	
     done
-    
 fi
 
-
+if [[ $1 == "" ]]; then
+    for i in "${era_list[@]}"
+    do
+        SKFlat.py -a $analyzer  -l $datapath/DATA_${i}.txt  -n ${njobs_data}  --nmax ${nmax}   -e ${i}    &
+        SKFlat.py -a $analyzer  -l $datapath/DATA_l_${i}.txt   -n ${njobs_data}  --nmax ${nmax}    -e ${i} &
+        SKFlat.py -a $analyzer  -l ${mcpath}/MCXSmall.txt  -n 2  --nmax ${nmax}   -e ${i}  &
+        SKFlat.py -a $analyzer  -l ${mcpath}/MCSmall.txt  -n 5  --nmax ${nmax}   -e ${i}  &
+        SKFlat.py -a $analyzer  -l ${mcpath}/MCMedium.txt  -n 20  --nmax ${nmax}   -e ${i}  
+        SKFlat.py -a $analyzer  -l ${mcpath}/MCLarge.txt  -n 50  --nmax ${nmax}   -e ${i}   &
+        SKFlat.py -a $analyzer  -l ${mcpath}/MCXLarge.txt  -n 200  --nmax ${nmax}   -e ${i}   &
+        SKFlat.py -a $analyzer  -l ${mcpath}/${i}/QCD.txt -n 2  --nmax ${nmax}   -e ${i} 
+    done
+fi
