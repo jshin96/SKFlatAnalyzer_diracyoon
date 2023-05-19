cf757870aedd4a5d9bf5330383add46fa92f04a5
jalmond <jalmond@cern.ch>
2023-05-19 17:45:48 +0900

    Update Fake skims

--- SkimScripts/SkimTree_FakeEventSkimBDT/runSkimTree_FakeEventSkim.sh
+++ SkimScripts/SkimTree_FakeEventSkimBDT/runSkimTree_FakeEventSkim.sh
@@ -3,13 +3,14 @@ rundir=runSkims
 mcpath=${SKFlat_WD}/SkimScripts/${analyzer}/Bkg/
 njobs=600
 njobs_data=600
-nmax=50
+nmax=350
 skim=' '
 declare  -a era_list=("2018" "2017"  "2016preVFP" "2016postVFP")
 if [[ $1 == "" ]]; then
     for i in "${era_list[@]}"
     do
-	SKFlat.py -a $analyzer  -l  $mcpath/QCD_${i}.txt  -n 400  --nmax ${nmax}   -e ${i}  --skim SkimTree_FakeEventSkim
+	SKFlat.py -a $analyzer  -l  $mcpath/QCD_${i}.txt  -n 600  --nmax ${nmax}   -e ${i}  --skim SkimTree_FakeEventSkim&
+	SKFlat.py -a $analyzer  -l  $mcpath/MC.txt  -n 600  --nmax ${nmax}   -e ${i}  --skim SkimTree_FakeEventSkim&
     done
 
 fi
