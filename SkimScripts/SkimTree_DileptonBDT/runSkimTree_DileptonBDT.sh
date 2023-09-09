729b87811b291cff6d1d8af57fd528d014e24145
jalmond <jalmond@cern.ch>
2023-09-09 21:50:04 +0900

    tidy sumbittion scripts

--- SkimScripts/SkimTree_DileptonBDT/runSkimTree_DileptonBDT.sh
+++ SkimScripts/SkimTree_DileptonBDT/runSkimTree_DileptonBDT.sh
@@ -7,6 +7,7 @@ njobs=600
 njobs_data=600
 nmax=400
 skim=' '
+
 declare  -a era_list=("2018" "2017"  "2016preVFP" "2016postVFP")
 
 if [[ $1 == "" ]]; then
@@ -14,19 +15,8 @@ if [[ $1 == "" ]]; then
     declare  -a era_list=( "2016preVFP" "2016postVFP")
     for i in "${era_list[@]}"
     do
-        SKFlat.py -a $analyzer  -l $mcpath/MC2.txt   --nmax 400           -e ${i} &
+        SKFlat.py -a $analyzer  -l $mcpath/MC_Additional.txt   --nmax 300           -e ${i} &
       done
-
-fi
-
-if [[ $1 == "Ind" ]]; then
-    
-    declare  -a era_list=("2018" )
-    
-     for i in "${era_list[@]}"
-     do
-	 SKFlat.py -a $analyzer  -l  $datapath/DATA_${i}_ind.txt  -n 500  --nmax 300    -e ${i} --skim SkimTree_Dilepton& 
-     done
 
 fi
 
@@ -37,10 +27,10 @@ if [[ $1 == "All" ]]; then
     for i in "${era_list[@]}"
     
     do
-        SKFlat.py -a $analyzer  -l $datapath/DATA_${i}.txt  -n ${njobs_data}  --nmax ${nmax}           -e ${i}  --skim SkimTree_DileptonBDT  &
-	SKFlat.py -a $analyzer  -l $datapath/${i}_SingleLepton.txt  -n ${njobs_data}  --nmax ${nmax}   -e ${i}  --skim SkimTree_DileptonBDT  &
-	SKFlat.py -a $analyzer  -l $datapath/DATA_${i}EMu.txt  -n ${njobs_data}  --nmax ${nmax}        -e ${i} --skim SkimTree_DileptonBDT  &
-	SKFlat.py -a $analyzer  -l $mcpath/MC.txt  -n 2000  --nmax ${nmax}   -e ${i}  --skim SkimTree_DileptonBDT &
+        SKFlat.py -a $analyzer  -l $datapath/DATA_${i}.txt               -n ${njobs_data}  --nmax ${nmax}   -e ${i}  --skim SkimTree_DileptonBDT &
+	SKFlat.py -a $analyzer  -l $datapath/DATA_${i}_EMu.txt           -n ${njobs_data}  --nmax ${nmax}   -e ${i}  --skim SkimTree_DileptonBDT &
+	SKFlat.py -a $analyzer  -l $datapath/DATA_${i}_SingleLepton.txt  -n ${njobs_data}  --nmax ${nmax}   -e ${i}  --skim SkimTree_DileptonBDT &
+	SKFlat.py -a $analyzer  -l $mcpath/MC.txt                        -n 2000           --nmax ${nmax}   -e ${i}  --skim SkimTree_DileptonBDT &
     done
 fi
 
