ff9ba95c937f3d304a3f65519fe075270bfd1293
jalmond <jalmond@cern.ch>
2023-10-26 14:59:30 +0900

    Update Fake skims

--- SkimScripts/SkimTree_HNFakeBDT/runSkimTree_HNFakeBDT.sh
+++ SkimScripts/SkimTree_HNFakeBDT/runSkimTree_HNFakeBDT.sh
@@ -4,7 +4,7 @@ mcpath=${SKFlat_WD}/SkimScripts/${analyzer}/Bkg/
 datapath=${SKFlat_WD}/SkimScripts/${analyzer}/data_lists_multilep/
 njobs=400
 njobs_data=400
-nmax=350
+nmax=400
 skim=' '
 declare  -a era_list=("2016postVFP" "2016preVFP" "2017" "2018")
 
@@ -12,7 +12,7 @@ if [[ $1 == "" ]]; then
     for i in "${era_list[@]}"
     do
         SKFlat.py -a $analyzer  -l $datapath/DATA_${i}_mu.txt  -n ${njobs_data}  --nmax ${nmax}   -e ${i} --skim SkimTree_HNFakeBDT&
-        SKFlat.py -a $analyzer  -l $datapath/DATA_${i}_el.txt  -n ${njobs_data}  --nmax ${nmax}   -e ${i} --skim SkimTree_HNFakeBDT&
+        #SKFlat.py -a $analyzer  -l $datapath/DATA_${i}_el.txt  -n ${njobs_data}  --nmax ${nmax}   -e ${i} --skim SkimTree_HNFakeBDT&
 	SKFlat.py -a $analyzer  -l $mcpath/MC2.txt             -n ${njobs}       --nmax ${nmax}   -e ${i} --skim SkimTree_HNFakeBDT&
     done
 fi
