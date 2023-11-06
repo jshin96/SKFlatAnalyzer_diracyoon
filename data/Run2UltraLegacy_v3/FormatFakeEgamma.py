12b1e06d2f37bf184d7a47e95864e6178677466f
jalmond <jalmond@cern.ch>
2023-11-06 21:57:04 +0900

    clean up names of FR Files

---
+++ data/Run2UltraLegacy_v3/FormatFakeEgamma.py
@@ -0,0 +1,30 @@
+import os 
+
+print ("Moving 2016a FR files")
+MV2016a="mv /data6/Users/jalmond/2020/HNDiLeptonWorskspace/src/HNL_LeptonFakes/CreateInput/EGamma/FR_EGamma_2016a_* 2016preVFP/FakeRate/DataFR/ElFR/"
+MV2016b="mv /data6/Users/jalmond/2020/HNDiLeptonWorskspace/src/HNL_LeptonFakes/CreateInput/EGamma/FR_EGamma_2016b_* 2016postVFP/FakeRate/DataFR/ElFR/"
+MV2017 ="mv /data6/Users/jalmond/2020/HNDiLeptonWorskspace/src/HNL_LeptonFakes/CreateInput/EGamma/FR_EGamma_2017_*  2017/FakeRate/DataFR/ElFR/"
+MV2018 ="mv /data6/Users/jalmond/2020/HNDiLeptonWorskspace/src/HNL_LeptonFakes/CreateInput/EGamma/FR_EGamma_2018_*  2018/FakeRate/DataFR/ElFR/"
+
+MVPr2016a="mv /data6/Users/jalmond/2020/HNDiLeptonWorskspace/src/HNL_LeptonFakes/CreateInput/EGamma/PR_EGamma_2016a_HNL_ULID.root 2016preVFP/FakeRate/DataFR/ElFR/"
+MVPr2016b="mv /data6/Users/jalmond/2020/HNDiLeptonWorskspace/src/HNL_LeptonFakes/CreateInput/EGamma/PR_EGamma_2016b_HNL_ULID.root 2016postVFP/FakeRate/DataFR/ElFR/"
+MVPr2017 ="mv /data6/Users/jalmond/2020/HNDiLeptonWorskspace/src/HNL_LeptonFakes/CreateInput/EGamma/PR_EGamma_2017_HNL_ULID.root 2017/FakeRate/DataFR/ElFR/"
+MVPr2018 ="mv /data6/Users/jalmond/2020/HNDiLeptonWorskspace/src/HNL_LeptonFakes/CreateInput/EGamma/PR_EGamma_2018_HNL_ULID.root 2018/FakeRate/DataFR/ElFR/"
+
+os.system(MV2016a)
+os.system(MVPr2016a)
+os.system("ls -lth 2016preVFP/FakeRate/DataFR/ElFR/")
+
+os.system(MV2016b)
+os.system(MVPr2016b)
+os.system("ls -lth 2016postVFP/FakeRate/DataFR/ElFR/")
+
+os.system(MV2017)
+os.system(MVPr2017)
+os.system("ls -lth 2017/FakeRate/DataFR/ElFR/")
+
+os.system(MV2018)
+os.system(MVPr2018)
+os.system("ls -lth 2018/FakeRate/DataFR/ElFR/")
+
+
