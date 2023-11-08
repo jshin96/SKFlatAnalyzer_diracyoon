5dcd256c086d5235acb7275d27359b4f29318c0f
John Almond <jalmond@cern.ch>
2023-11-08 15:48:17 +0900

    Update HNL_LeptonCore.C

--- Analyzers/src/HNL_LeptonCore.C
+++ Analyzers/src/HNL_LeptonCore.C
@@ -406,7 +406,7 @@ AnalyzerParameter HNL_LeptonCore::SetupHNLParameter(TString s_setup_version, TSt
   param.PlottingVerbose = 0;
 
   /// Default BKG 
-  param.ApplyPR;
+  param.ApplyPR=false;
 
   param.Apply_Weight_Norm1Ipb  = true;
   param.Apply_Weight_LumiNorm = true;
