d800df81a9fe7357592b95190eec79e182f6e39a
jalmond <jalmond@cern.ch>
2023-11-09 20:21:45 +0900

    Add SkimTree_HNMultiLep_SingleMuon skims

--- SkimScripts/SkimTree_FakeEventSkimBDT/runSkimTree_FakeEventSkim.sh
+++ SkimScripts/SkimTree_FakeEventSkimBDT/runSkimTree_FakeEventSkim.sh
@@ -3,19 +3,18 @@ rundir=runSkims
 mcpath=${SKFlat_WD}/SkimScripts/${analyzer}/Bkg/
 njobs=600
 njobs_data=600
-nmax=350
+nmax=500
 skim=' '
 declare  -a era_list=("2018" "2017"  "2016preVFP" "2016postVFP")
-
-declare  -a era_list=(  "2016preVFP" "2016postVFP")
+declare  -a era_list=("2017")
 
 if [[ $1 == "" ]]; then
     for i in "${era_list[@]}"
     do
 	
-	#SKFlat.py -a $analyzer  -l  $mcpath/QCD_${i}.txt    -n 20  --nmax ${nmax}   -e ${i}  --skim SkimTree_FakeEventSkimBDT&
-        #SKFlat.py -a $analyzer  -l  $mcpath/HaneolList.txt  -n 100  --nmax ${nmax}   -e ${i}  --skim SkimTree_FakeEventSkimBDT&
-	SKFlat.py -a $analyzer  -l  $mcpath/MC.txt          -n 100  --nmax ${nmax}   -e ${i}  --skim SkimTree_FakeEventSkimBDT&
+        SKFlat.py -a $analyzer  -l  $mcpath/QCD_${i}Mu.txt  -n 400  --nmax ${nmax}   -e ${i} --skim SkimTree_FakeEventSkim  &
+        SKFlat.py -a $analyzer  -l  $mcpath/MC.txt  -n       400  --nmax ${nmax}   -e ${i} --skim SkimTree_FakeEventSkim&
+        SKFlat.py -a $analyzer  -l  $mcpath/QCD_BEnriched.txt -n 400  --nmax ${nmax}   -e ${i} --skim SkimTree_FakeEventSkim &
 
     done
 
--- SkimScripts/SkimTree_HNMultiLepBDT/runSkimTree_HNMultilepBDT.sh
+++ SkimScripts/SkimTree_HNMultiLepBDT/runSkimTree_HNMultilepBDT.sh
@@ -5,11 +5,18 @@ datapath=${SKFlat_WD}/SkimScripts/${analyzer}/data_lists_multilep/
 sigpath=${SKFlat_WD}/SkimScripts/${analyzer}/Signals/
 njobs=600
 njobs_data=100
-nmax=400
+nmax=500
 skim=' '
 
 declare  -a era_list=("2016postVFP" "2016preVFP"  "2017" "2018")
 
+
+if [[ $1 == "SL" ]]; then
+    for i in "${era_list[@]}"
+    do
+        SKFlat.py -a $analyzer  -l $datapath/DATA_mu_${i}.txt   -n ${njobs_data}  --nmax 500    -e ${i} --skim  SkimTree_HNMultiLep &
+    done
+fi
 
 
 if [[ $1 == "DATA" ]]; then
@@ -18,8 +25,8 @@ if [[ $1 == "DATA" ]]; then
 
     for i in "${era_list[@]}"
     do
-        SKFlat.py -a $analyzer  -l $datapath/DATA_${i}.txt     -n ${njobs_data}  --nmax ${nmax}    -e ${i} --skim  SkimTree_HNMultiLepBDT &
-        SKFlat.py -a $analyzer  -l $datapath/DATA_l_${i}.txt   -n ${njobs_data}  --nmax ${nmax}    -e ${i} --skim  SkimTree_HNMultiLepBDT &
+        SKFlat.py -a $analyzer  -l $datapath/DATA_${i}.txt     -n ${njobs_data}  --nmax ${nmax}    -e ${i} --skim  SkimTree_HNMultiLep &
+        SKFlat.py -a $analyzer  -l $datapath/DATA_l_${i}.txt   -n ${njobs_data}  --nmax ${nmax}    -e ${i} --skim  SkimTree_HNMultiLep &
     done
 fi
 
@@ -65,8 +72,8 @@ if [[ $1 == "MC3" ]]; then
 
     for i in "${era_list[@]}"
     do
-        SKFlat.py -a $analyzer  -l ${mcpath}/MCLarge.txt   -n 50   --nmax ${nmax}   -e ${i}   --skim  SkimTree_HNMultiLepBDT &
-        SKFlat.py -a $analyzer  -l ${mcpath}/MCXLarge.txt  -n 200  --nmax ${nmax}   -e ${i}   --skim  SkimTree_HNMultiLepBDT &
+        SKFlat.py -a $analyzer  -l ${mcpath}/MCLarge.txt   -n 50   --nmax ${nmax}   -e ${i}   --skim  SkimTree_HNMultiLep &
+        SKFlat.py -a $analyzer  -l ${mcpath}/MCXLarge.txt  -n 200  --nmax ${nmax}   -e ${i}   --skim  SkimTree_HNMultiLep &
     done
 fi
 
@@ -77,14 +84,14 @@ if [[ $1 == "" ]]; then
 
     for i in "${era_list[@]}"
     do
-        SKFlat.py -a $analyzer  -l $datapath/DATA_${i}.txt     -n ${njobs_data}  --nmax ${nmax}   -e ${i} --skim  SkimTree_HNMultiLepBDT &
-        SKFlat.py -a $analyzer  -l $datapath/DATA_l_${i}.txt   -n ${njobs_data}  --nmax ${nmax}   -e ${i} --skim  SkimTree_HNMultiLepBDT &
-        SKFlat.py -a $analyzer  -l ${mcpath}/MCXSmall.txt      -n 2              --nmax ${nmax}   -e ${i} --skim  SkimTree_HNMultiLepBDT &
-        SKFlat.py -a $analyzer  -l ${mcpath}/MCSmall.txt       -n 5              --nmax ${nmax}   -e ${i} --skim  SkimTree_HNMultiLepBDT &
-        SKFlat.py -a $analyzer  -l ${mcpath}/MCMedium.txt      -n 20             --nmax ${nmax}   -e ${i} --skim  SkimTree_HNMultiLepBDT &
-        SKFlat.py -a $analyzer  -l ${mcpath}/MCLarge.txt       -n 50             --nmax ${nmax}   -e ${i} --skim  SkimTree_HNMultiLepBDT &
-        SKFlat.py -a $analyzer  -l ${mcpath}/MCXLarge.txt      -n 200            --nmax ${nmax}   -e ${i} --skim  SkimTree_HNMultiLepBDT &
-        SKFlat.py -a $analyzer  -l ${mcpath}/${i}/QCD.txt      -n 2              --nmax ${nmax}   -e ${i} --skim  SkimTree_HNMultiLepBDT &
-        SKFlat.py -a $analyzer  -l ${sigpath}/Signal.txt       -n 10             --nmax ${nmax}   -e ${i} --skim  SkimTree_HNMultiLepBDT &
+        SKFlat.py -a $analyzer  -l $datapath/DATA_${i}.txt     -n ${njobs_data}  --nmax ${nmax}   -e ${i} --skim  SkimTree_HNMultiLep&
+        SKFlat.py -a $analyzer  -l $datapath/DATA_l_${i}.txt   -n ${njobs_data}  --nmax ${nmax}   -e ${i} --skim  SkimTree_HNMultiLep&
+        SKFlat.py -a $analyzer  -l ${mcpath}/MCXSmall.txt      -n 2              --nmax ${nmax}   -e ${i} --skim  SkimTree_HNMultiLep&
+        SKFlat.py -a $analyzer  -l ${mcpath}/MCSmall.txt       -n 5              --nmax ${nmax}   -e ${i} --skim  SkimTree_HNMultiLep&
+        SKFlat.py -a $analyzer  -l ${mcpath}/MCMedium.txt      -n 20             --nmax ${nmax}   -e ${i} --skim  SkimTree_HNMultiLep&
+        SKFlat.py -a $analyzer  -l ${mcpath}/MCLarge.txt       -n 50             --nmax ${nmax}   -e ${i} --skim  SkimTree_HNMultiLep&
+        SKFlat.py -a $analyzer  -l ${mcpath}/MCXLarge.txt      -n 200            --nmax ${nmax}   -e ${i} --skim  SkimTree_HNMultiLep&
+        SKFlat.py -a $analyzer  -l ${mcpath}/${i}/QCD.txt      -n 2              --nmax ${nmax}   -e ${i} --skim  SkimTree_HNMultiLep&
+        SKFlat.py -a $analyzer  -l ${sigpath}/Signal.txt       -n 10             --nmax ${nmax}   -e ${i} --skim  SkimTree_HNMultiLep&
     done
 fi
