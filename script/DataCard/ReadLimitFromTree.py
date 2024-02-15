28839e72a1ced6757d9a70a72d9477eeb709a76c
Hooooon12 <theking461@snu.ac.kr>
2024-02-15 21:34:27 +0900

    240129_JetsX_Limit

--- script/DataCard/ReadLimitFromTree.py
+++ script/DataCard/ReadLimitFromTree.py
@@ -9,22 +9,23 @@ parser.add_argument('--Asymptotic', action='store_true')
 parser.add_argument('--Full', action='store_true')
 args = parser.parse_args()
 
-workdir = "/data6/Users/jihkim/CombineTool/CMSSW_10_2_13/src/DataCardsShape/HNL_SignalRegionPlotter/Batch/" #FIXME where your batch outputs are stored.
+workdir = "/data6/Users/jihkim/CombineTool/CMSSW_10_2_13/src/DataCardsShape/HNL_SignalRegionPlotter/Batch/"
 
 years = ["2016","2017","2018"]
 years = ["Run2"]
-years = ["2017"]
 years = ["2016preVFP","2016postVFP","2017","2018","Run2"]
 years = ["Run2"]
+years = ["2017"]
 channels = ["MuMu","EE","EMu"]
-channels = ["MuMu","EE"]
+#channels = ["MuMu","EE"]
 #channels = ["MuMu"]
-masses = ["100","200","300","400","500","600","700","800","900","1000","1100","1200","1300","1500","1700","2000","2500","3000"]
-masses = ["90","100","200","300","400","500","600","700","800","900","1000","1100","1200","1300","1500","1700","2000","2500","3000","5000","7500","10000","15000","20000"]
-masses = ["90","100","200","300","400","1000","10000"]
-#masses = ["500",]
+#masses = ["100","200","300","400","500","600","700","800","900","1000","1100","1200","1300","1500","1700","2000","2500","3000"]
+#masses = ["90","100","150","200","300","400","500","600","700","800","900","1000","1100","1200","1300","1500","1700","2000","2500","3000","5000","7500","10000","15000","20000"]
+masses = ["100","150","200","300","400","500","600","700","800","900","1000","1100","1200","1300","1500","1700","2000","2500","3000","5000","7500","10000","15000","20000"]
+#masses = ["90","100","200","300","400","1000","10000"]
 IDs = [""] #["_ID"]
-tags = [""] #["_DYVBF"]
+#tags = ["","_sronly"] #["_DYVBF"]
+tags = ["_syst"] #["_DYVBF"]
 #myWPs = ["Workshop", "InputForCombine"]
 #myWPs = ["InputForCombine"]
 #myWPs = ["Workshop"]
@@ -34,8 +35,11 @@ tags = [""] #["_DYVBF"]
 #myWPs = ["SR2HT_SR3l2pt"]
 #myWPs = ["SR2HT_SR3l2pt_ChargeSplit"]
 #myWPs = ["HNL_ULID","HNTightV2"]
-myWPs = ["NewOpt_HNL_ULID","NewOpt_HNTightV2"]
-myWPs = ["NewOpt_HNL_ULID_FullCLs"]
+#myWPs = ["NewOpt_HNL_ULID","NewOpt_HNTightV2"]
+#myWPs = ["NewOpt_HNL_ULID_FullCLs"]
+#myWPs = ["231227_KCMS_WS_HNL_ULID","231227_KCMS_WS_HNTightV2"]
+#myWPs = ["CRtest_HNL_ULID"]
+myWPs = ["CRtest_HNL_ULID_Syst"]
 
 for WP in myWPs:
   this_workdir = workdir+WP
@@ -43,7 +47,9 @@ for WP in myWPs:
   for year, channel, ID, tag in [[year, channel, ID, tag] for year in years for channel in channels for ID in IDs for tag in tags]:
     
     if args.Asymptotic:
-      with open("out/"+WP+"/"+year+"_"+channel+ID+tag+"_Asym_limit.txt", 'w') as f:
+      #with open("out/"+WP+"/"+year+"_"+channel+ID+tag+"_Asym_limit.txt", 'w') as f:
+      #with open("out/"+WP+"/"+year+"_"+channel+ID+tag+"_Run2Scaled_Asym_limit.txt", 'w') as f:
+      with open("out/"+WP+"/"+year+"_"+channel+ID+tag+"_Run23Scaled_Asym_limit.txt", 'w') as f:
   
         for mass in masses:
           this_name = year+"_"+channel+"_M"+mass+ID+tag
@@ -53,13 +59,15 @@ for WP in myWPs:
           tree_Asym = f_Asym.Get("limit")
   
           tree_Asym.GetEntry(2) # substitute for obs. limit for now
-          f.write(mass+"\t"+str(round(tree_Asym.limit,3))+"\t")
+          #f.write(mass+"\t"+str(round(tree_Asym.limit,3))+"\t")
           #f.write(mass+"\t"+str(round(tree_Asym.limit/1.87,3))+"\t") # FIXME estimating full Run2 from 2017
+          f.write(mass+"\t"+str(round(tree_Asym.limit/3.16,3))+"\t") # FIXME estimating full Run2+3 from 2017
   
           for i in range(5): # expected limits
             tree_Asym.GetEntry(i)
-            f.write(str(round(tree_Asym.limit,3))+"\t")
+            #f.write(str(round(tree_Asym.limit,3))+"\t")
             #f.write(str(round(tree_Asym.limit/1.87,3))+"\t") # FIXME estimating full Run2 from 2017
+            f.write(str(round(tree_Asym.limit/3.16,3))+"\t") # FIXME estimating full Run2+3 from 2017
           f.write("\n")
   
     if args.Full:
